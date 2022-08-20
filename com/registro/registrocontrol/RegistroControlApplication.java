e6c3e22e76d00ad23a643220e878e28fd8973d59
“DelvisVides” <delvisvides92@gmail.com>
2022-08-20 15:43:56 -0500

    Se agregó una nueva rama develop

---
+++ com/registro/registrocontrol/RegistroControlApplication.java
@@ -0,0 +1,13 @@
+package com.registro.registrocontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroControlApplication.class, args);
+    }
+
+}
